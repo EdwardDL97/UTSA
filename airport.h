b50e000e78390905bcfbd33233745f55567afb6b
EdwardDL97 <50053212+EdwardDL97@users.noreply.github.com>
2019-06-24 13:02:31 -0500

    Update airport.h

--- airport.h
+++ airport.h
@@ -2,11 +2,13 @@
    Project 1
    Edward Diaz Lopez
 */
+
 #ifndef AIRPORT_H
 #define AIRPORT_H
 #define MAX 50
 #define PI 3.14159
 
+/*defines struct with airport code, name, latitude and longitude*/
 typedef struct Airport
 {
   char code [4];
@@ -15,6 +17,7 @@ typedef struct Airport
   double longitude;
 }Airport;
 
+/*All the prototypes that are going to be in the airport-program.c and airport.c*/
 void printAirports (Airport [MAX], int);
 void printAirport(Airport);
 Airport findAirport(Airport[MAX] , int, char [4]);
