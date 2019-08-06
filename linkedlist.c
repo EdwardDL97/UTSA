6158dacad6800ab64c540c960dad2e4de72ab802
EdwardDL97 <50053212+EdwardDL97@users.noreply.github.com>
2019-08-06 13:28:20 -0500

    update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,81 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "linkedlist.h"
+#include "itinerary.h"
 
-Node *insertNode( Node *head, Node * newNode )
+Destination *create( char *code )
 {
-    Node *ptr;
-    Node *previous = NULL;
+  Destination *ptr = (Destination *)malloc( sizeof(Destination) );
+  strcpy(ptr->airportCode, code);
+  ptr->next = NULL;
 
-    if ( head == NULL )
-    {	
-	return newNode;
-    }
-
-    for( ptr = head; ptr != NULL; ptr = ptr->next )
-    {
-	if( strcmp( ptr->name, newNode->name ) > 0)
-	{
-	    newNode-> next = ptr;
-	    if (previous == NULL )
-	    {
-	        previous->next = newNode;
-		return head;
-	    }
-	    else
-	    {
-		return newNode;
-	    }
-		
-
-    }
-	previous = ptr;
-  }
-   
+  return ptr;
 }
 
-Node *findNode( Node *head, int key )
+Destination *insertAfter( Destination *head, Destination *node, char *key )
 {
-    Node *ptr;
-    
-    for( ptr = head; ptr != NULL; ptr = ptr->next )
-    {
-        if( ptr->number == key )
-	{
-	  return ptr;
-	}
-    }
-    return NULL;
-}
-
-void print( Node *h )
-{
-    Node *ptr;
-   
-    for( ptr = h; ptr != NULL; ptr = ptr->next )
-    {
-        printf( "Value: %d %s\n", ptr->number, ptr->name );
-    }
-}
-
-Node * createNode( int number, char *name )
-{
-  Node *n = (Node *) malloc( sizeof( Node ) );
-  n ->number = number;
-  strcpy( n->name, name );
-  n->next = NULL;
-
-  return n;
-}
-
-void printReverse( Node *n )
-{
-  if( n == NULL )
+  Destination *ptr;
+  if(strcmp(key,"ZZZ") == 0)
   {
-    return;
-  }
+      strcpy(key, node->airportCode);
+      node->next = head;
+      head = node;
 
-  printReverse( n->next );
-  printf("%s\n", n->name );
+      return head;
+  }
+  if( head == NULL )
+  {
+    head = node;
+  }
+  else
+  {
+  for (ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+      if (strcmp(ptr->airportCode, key) == 0)
+      {
+        node->next = ptr->next;
+
+        ptr->next = node;
+      }
+    }
+  }
+  return head;
 }
+
+Destination *find( Destination *head, char *key )
+{
+  Destination *ptr;
+
+  for( ptr = head; ptr != NULL; ptr = ptr->next )
+  {
+    if ( strcmp(ptr->airportCode, key) == 0 )
+    {
+	return ptr;
+    }
+  }
+  return NULL;
+}
+
+Destination *removeNode( Destination *head, char *key )
+{
+  Destination *ptr;
+  Destination *previous;
+
+  for(ptr = head; ptr != NULL; ptr = ptr->next )
+  {
+    if( strcmp( ptr, key ) == 0 )
+    {
+	Destination *temp = ptr;
+
+	if( ptr == head )
+	{
+	  head = head->next;
+	}
+        else
+        {
+	  previous->next = ptr->next;
+        }
+        free( temp );
+        return head;
+    }
+    previous = ptr;
+  }
+  return head;
+}
+
+void print( Destination *head )
+{
+  Destination *d;
+
+  for ( d = head; d != NULL; d = d->next )
+  {
+    printf( "%s\n", d->airportCode );
+  }
+  printf("\n");
+}
+
+void printItinerary( Destination *head )
+{
+  Destination *i;
+  int counter = 0;
+
+  for (i = head; i != NULL; i = i->next )
+  {
+    if(i->next->airportCode == NULL)
+    {
+        break;
+    }
+        printf( "%s-%s\n", i->airportCode, i->next->airportCode );
+    counter ++;
+  }
+  printf("Total Segments: %d\n", counter);
+}
+
+Destination *destruct( Destination *head )
+{
+    Destination *temp;
+
+    while(head != NULL)
+    {
+        temp = head;
+        head = head->next;
+
+        free(temp);
+    }
+return NULL;
+}
+
