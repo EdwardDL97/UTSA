05f3644b616b2cd3801fe59f97a6a2dc491e4d60
EdwardDL97 <50053212+EdwardDL97@users.noreply.github.com>
2019-08-06 13:39:24 -0500

    updated itinerary.c

--- itinerary.c
+++ itinerary.c
@@ -7,33 +7,53 @@ Destination *create( char *code )
 {
   Destination *ptr = (Destination *)malloc( sizeof(Destination) );
   strcpy(ptr->airportCode, code);
-  strcpy(ptr->next, NULL);
+  ptr->next = NULL;
 
   return ptr;
 }
-/*
+
 Destination *insertAfter( Destination *head, Destination *node, char *key )
 {
   Destination *ptr;
-  Destination *previous = NULL;
-
-  while( head == NULL )
+  if(strcmp(key,"ZZZ") == 0)
   {
-	return node;
+      strcpy(key, node->airportCode);
+      node->next = head;
+      head = node;
+
+      return head;
   }
+  if( head == NULL )
+  {
+    head = node;
+  }
+  else
+  {
+  for (ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+      if (strcmp(ptr->airportCode, key) == 0)
+      {
+        node->next = ptr->next;
+
+        ptr->next = node;
+      }
+    }
+  }
+  return head;
 }
-*/
+
 Destination *find( Destination *head, char *key )
 {
   Destination *ptr;
- 
+
   for( ptr = head; ptr != NULL; ptr = ptr->next )
   {
-    if ( strcmp(head, key) == 0 )  
+    if ( strcmp(ptr->airportCode, key) == 0 )
     {
 	return ptr;
     }
   }
+  return NULL;
 }
 
 Destination *removeNode( Destination *head, char *key )
@@ -46,7 +66,7 @@ Destination *removeNode( Destination *head, char *key )
     if( strcmp( ptr, key ) == 0 )
     {
 	Destination *temp = ptr;
-    
+
 	if( ptr == head )
 	{
 	  head = head->next;
@@ -54,7 +74,7 @@ Destination *removeNode( Destination *head, char *key )
         else
         {
 	  previous->next = ptr->next;
-        } 
+        }
         free( temp );
         return head;
     }
@@ -71,14 +91,37 @@ void print( Destination *head )
   {
     printf( "%s\n", d->airportCode );
   }
+  printf("\n");
 }
 
 void printItinerary( Destination *head )
 {
   Destination *i;
+  int counter = 0;
 
-  for (i = head; i !=NULL; i = i->next )
+  for (i = head; i != NULL; i = i->next )
   {
-    printf( "%s-%s\n", i->airportCode, i->airportCode );
+    if(i->next->airportCode == NULL)
+    {
+        break;
+    }
+        printf( "%s-%s\n", i->airportCode, i->next->airportCode );
+    counter ++;
   }
+  printf("Total Segments: %d\n", counter);
 }
+
+Destination *destruct( Destination *head )
+{
+    Destination *temp;
+
+    while(head != NULL)
+    {
+        temp = head;
+        head = head->next;
+
+        free(temp);
+    }
+return NULL;
+}
+
